e8000efe088111170c3623aab5e294ee505cc802
Alessio Lovato <alessio.lovato.1@studenti.unipd.it>
2024-10-23 15:29:06 +0200

    Switched to composable nodes for the apriltag detection. This will prevent the publishment of the kinect image to the network, using unnecessary bandwidth.
    Added submodules to get the apriltag pose.
    
    With this new 'apriltag_ros' repo, the orientation of the tag is the same of the apriltag library, so it has to be rotated. This is done using a for cycle in the 'remote.launch.py' that publish the rotated pose.
    
    The config file has to be changed to a new format.

--- src/simulation/launch/remote.launch.py
+++ src/simulation/launch/remote.launch.py
@@ -1,94 +1,48 @@
+import os
+import yaml
+
 from ament_index_python.packages import get_package_share_directory
+
 from launch_ros.substitutions import FindPackageShare
-from launch_ros.actions import Node
+from launch_ros.actions import Node, ComposableNodeContainer, LoadComposableNodes
+from launch_ros.descriptions import ComposableNode
 from launch import LaunchDescription
-from launch.actions import (
-    DeclareLaunchArgument,
-    IncludeLaunchDescription,
-    OpaqueFunction,
-    GroupAction,
-    SetEnvironmentVariable,
-    RegisterEventHandler,
-    TimerAction
-)
-from launch.event_handlers import OnProcessExit, OnProcessStart
-from launch.conditions import IfCondition, LaunchConfigurationEquals
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import (
-    EnvironmentVariable,
-    LaunchConfiguration,
-    PathJoinSubstitution,
-    PythonExpression,
-    TextSubstitution
-)
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition, LaunchConfigurationEquals, LaunchConfigurationNotEquals
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 
 
 def generate_launch_description():
 
-############################################################################################################
-############################################  APRILTAG  ####################################################
-############################################################################################################
-
-    # Create apriltag node for camera 1
-    apriltag_node_1 = Node(
-        package='apriltag_ros',
-        executable='apriltag_node',
-        name='apriltag_node_1',
-        parameters= [os.path.join(get_package_share_directory('simulation'), 'config', 'apriltag.yaml'),
-                        {'publish_tag_detections_image': 'True'}],
-        remappings=[('camera_info', '/rgb/camera_info'),
-                    ('image_rect', '/rgb/image_raw'),
-                    ('/tf', '/tf_marker')],
-    )
-        
-        # Note to publish a static tf to link the marker to the locobot
-
-    apriltag_static_broadcaster = Node(
-        package='simulation',
-        executable='tf_publisher',
-        name='locobot_tag_static_tf_broadcaster',
-        parameters=[{'frame_id':'locobot/pan_link'},
-                    {'child_frame_id':'locobot_tag'},
-                    {'publish_frequency': 20.0},
-                    {'x':-0.10},
-                    {'y':0.0},
-                    {'z':0.1},
-                    {'roll':0.0},
-                    {'pitch':0.0},
-                    {'yaw':-1.57079632679}],
-        output='screen'
-    )
-
-    # Node that republish the tf from the apriltag_node_1 to the /tf topic of the locobot tag
-    locobot_remapper = Node(
-        package='simulation',
-        executable='tf_remapper',
-        name='tf_remapper_1',
-        namespace='locobot/env_camera_1',
-        output='screen',
-        parameters=[{'source_frame':'rgb_camera_link'},
-                    {'target_frame':'locobot_tag'},
-                    {'publish_frequency': 20.0}],
-        remappings=[('/tf', '/tf_marker'),
-                    ('/locobot/env_camera_1/tf_locobot', '/tf')]
-    )
-
-    # Node that republish the tf from the apriltag_node_1 to the /tf topic of the human tag
-    human_remapper = Node(
-        package='simulation',
-        executable='tf_remapper',
-        name='human_remapper',
-        namespace='locobot/env_camera_1',
-        output='screen',
-        parameters=[{'source_frame':'human_tag'},
-                    {'target_frame':'rgb_camera_link'},
-                    {'publish_frequency': 20.0}],,
-        remappings=[('/tf', '/tf_marker'),
-                    ('/locobot/env_camera_1/tf_locobot', '/tf')]
-    )
+    ld = LaunchDescription()
 
 ############################################################################################################
-############################################  RVIZ  ########################################################
+##########################################  CONFIGURATION  #################################################
+############################################################################################################
+
+      
+    # Declare the 'container' launch configuration
+    arg_container = DeclareLaunchArgument(
+        name='container', default_value='',
+        description=(
+            'Name of an existing node container to load launched nodes into. '
+            'If unset, a new container will be created.'
+        )
+    )
+
+    # Configuration file
+    config_file = os.path.join(get_package_share_directory('simulation'), 'config', 'apriltag.yaml')
+
+    # Camera
+    camera = 'k02'
+
+    # Topics
+    image_raw_topic = 'rgb/image_raw'
+    info_topic = 'rgb/camera_info'
+    image_rect_topic = 'rgb/image_rect'
+
+############################################################################################################
+###############################################  NODES  ####################################################
 ############################################################################################################
 
     # RViz2 node
@@ -98,10 +52,12 @@ def generate_launch_description():
         name='rviz2',
         arguments=['-d', PathJoinSubstitution([FindPackageShare("simulation"), 'rviz', 'navigation_3D.rviz']),
                    '--ros-args', '--log-level', 'warn'],
+        parameters=[{'use_sim_time': True}],
         output='screen'
     )
 
-    tf_camera_1 = Node(
+    # Static transform publisher for the map frame
+    map_tf = Node(
         package="tf2_ros",
         executable="static_transform_publisher",
         arguments = ['--x', '-0.303', '--y', '0.373', '--z', '3.071',
@@ -111,8 +67,92 @@ def generate_launch_description():
         output='screen'
     )
 
-    return LaunchDescription([
-        include_markers_launch,
-        rviz,
-        tf_camera_1
-    ])
+    # Node to publish a static tf to link the marker to the locobot base
+    locobot_apriltag_tf = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        arguments = ['--x', '0.0', '--y', '0.0', '--z', '-0.63',
+                     '--roll', '0.0', '--pitch', '0.0', '--yaw', '1.57079632679', 
+                     '--frame-id', 'locobot_tag_rotated',
+                     '--child-frame-id', 'locobot/base_footprint'],
+        output='screen'
+    )
+
+    # Azure Kinect ROS driver 
+    kinectNode = Node(
+        package='azure_kinect_ros_driver',
+        executable='node',
+        output='screen',
+        namespace=camera,
+        parameters=[
+            {'sensor_sn': '000071601712'}, # Serial number of the camera 'k02'
+            {'depth_enabled': False},
+            {'rgb_point_cloud': False},
+            {'color_enabled': True},
+            {'color_resolution': '1080P'},
+            {'fps': 30},
+            {'point_cloud': False},
+        ]
+    )
+
+    # Rotate the tf of the markers
+    with open(config_file, 'r') as stream:
+        yaml_data = yaml.safe_load(stream)
+        tag_frames = yaml_data['tag_frames']
+        for tag_frame in tag_frames:
+            ld.add_action(Node(
+                package='tf2_ros',
+                executable='static_transform_publisher',
+                output='screen' ,
+                arguments=['0', '0', '0', '3.14159265359', '-1.57079632679', '0', tag_frame, tag_frame + '_rotated']
+            ))
+
+    # Node composition of the image_proc node and the apriltag_ros node
+    composable_nodes = [
+        ComposableNode(
+            package='image_proc',
+            plugin='image_proc::RectifyNode',
+            name='rectify_rgb_node',
+            namespace=camera + '/rgb',
+            remappings=[  
+                ('image', 'image_raw')]
+        ),
+        ComposableNode(
+            name=LaunchConfiguration('container'),
+            namespace=camera + '/rgb',
+            package='apriltag_ros',
+            plugin='AprilTagNode',
+            parameters=[config_file],
+            remappings=[('image', 'image_raw')], #TODO: Change to image_rect when the rectification is working
+        )
+    ]
+
+    # If an existing container is not provided, start a container and load nodes into it
+    image_container = ComposableNodeContainer(
+        condition=LaunchConfigurationEquals('container', ''),
+        name='image_container',
+        namespace='',
+        package='rclcpp_components',
+        executable='component_container',
+        composable_node_descriptions=composable_nodes,
+        output='screen'
+    )
+
+    # If an existing container name is provided, load composable nodes into it
+    # This will block until a container with the provided name is available and nodes are loaded
+    load_composable_nodes = LoadComposableNodes(
+        condition=LaunchConfigurationNotEquals('container', ''),
+        composable_node_descriptions=composable_nodes,
+        target_container=LaunchConfiguration('container'),
+    )
+
+
+    ld.add_action(arg_container)
+    ld.add_action(rviz)
+    ld.add_action(map_tf)
+    ld.add_action(locobot_apriltag_tf)
+    ld.add_action(kinectNode)
+    ld.add_action(image_container)
+    ld.add_action(load_composable_nodes)
+
+    return ld
